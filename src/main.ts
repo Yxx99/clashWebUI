8c09c2ad9d4ab4d214d1b15460774078a7de312b
B4yg3qk <B4yg3qk@localhost.com>
2025-07-22 18:21:40 +0800

    feat: 更新首页标题和图标，添加 Pinia 状态管理，集成 Element Plus 图标组件，调整样式以简化布局。

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(router)
-
+app.use(createPinia())
 app.mount('#app')
